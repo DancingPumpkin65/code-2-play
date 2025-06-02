647804985ecbe562af64c555b1adb846ec07e7c6
Scripted Logic <ouassbader184@gmail.com>
2025-06-02 18:42:07 +0100

    Refactor TileMapRenderer to handle dynamic tile dimensions and update layer data structure; modify tileLayer1 format and add TilesetField XML file.

--- TEST/JS/CLASS/TileMapRenderer.js
+++ TEST/JS/CLASS/TileMapRenderer.js
@@ -6,24 +6,22 @@ class TileMapRenderer {
         this.tileSize = tileSize;
         this.mapCols = mapCols;
         this.mapRows = mapRows || (layers[0].data.length / mapCols) | 0;
-        this.layers   = layers;
+        this.layers = layers;
 
         const srcs = [...new Set(layers.map(l => l.tilesetSrc))];
         this.tilesetImages = {};
-        this.tilesPerRow = {};
         let loaded = 0;
         srcs.forEach(src => {
             const img = new Image();
             img.src = src;
             img.onload = () => {
-                this.tilesPerRow[src] = Math.floor(img.width / this.tileSize);
                 if (++loaded === srcs.length) this.renderAll();
             };
             this.tilesetImages[src] = img;
         });
 
-        this.canvas.width = this.mapCols * this.tileSize;
-        this.canvas.height = this.mapRows * this.tileSize;
+        this.canvas.width  = this.mapCols * (layers[0].tilewidth || tileSize);
+        this.canvas.height = this.mapRows * (layers[0].tileheight || tileSize);
     }
 
     renderAll() {
@@ -34,24 +32,29 @@ class TileMapRenderer {
         });
     }
 
-    renderLayer({ data, tilesetSrc, firstGid }) {
+    renderLayer({ data, tilesetSrc, firstGid, columns, tilewidth, tileheight }) {
         const img = this.tilesetImages[tilesetSrc];
-        const tpr = this.tilesPerRow[tilesetSrc];
+        const tpr = columns;
+        const tw  = tilewidth;
+        const th  = tileheight;
+
         for (let i = 0; i < data.length; i++) {
             const gid = data[i];
             if (!gid || gid < firstGid) continue;
             const localId = gid - firstGid + 1;
-            const col = i % this.mapCols;
-            const row = (i / this.mapCols) | 0;
-            const imgCol = (localId - 1) % tpr;
-            const imgRow = ((localId - 1) / tpr) | 0;
+            const col     = i % this.mapCols;
+            const row     = (i / this.mapCols) | 0;
+            const imgCol  = (localId - 1) % tpr;
+            const imgRow  = ((localId - 1) / tpr) | 0;
 
             this.ctx.drawImage(
                 img,
-                imgCol * this.tileSize, imgRow * this.tileSize,
-                this.tileSize, this.tileSize,
-                col * this.tileSize, row * this.tileSize,
-                this.tileSize, this.tileSize
+                imgCol * tw, imgRow * th,
+                tw, th,
+                col  * this.tileSize,
+                row  * this.tileSize,
+                this.tileSize,
+                this.tileSize
             );
         }
     }
--- TEST/JS/test.js
+++ TEST/JS/test.js
@@ -3,7 +3,31 @@ const renderer = new TileMapRenderer({
     tileSize: 16,
     mapCols: 19,
     layers: [
-        { data: tileLayer3, tilesetSrc: 'TILESETS/TilesetFloor.png', firstGid: 580 },
-        { data: tileLayer2, tilesetSrc: 'TILESETS/TilesetField.png', firstGid: 1 }
+        {
+            name:        'Floor',
+            tilesetSrc:  'TILESETS/TilesetFloor.png',
+            tilewidth:   16,
+            tileheight:  16,
+            tilecount:   572,
+            columns:     22,
+            width:       352,
+            height:      417,
+            firstGid:    580,
+            data:        tileLayer3,
+            collision:   false
+        },
+        {
+            name:        'Field',
+            tilesetSrc:  'TILESETS/TilesetField.png',
+            tilewidth:   16,
+            tileheight:  16,
+            tilecount:   75,
+            columns:     5,
+            width:       80,
+            height:      240,
+            firstGid:    1,
+            data:        tileLayer2,
+            collision:   false
+        }
     ]
 });
